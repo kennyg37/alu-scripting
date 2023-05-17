bd24bc7a0fee3175a3e016634d2aebfd82d6a156
kennyg37 <k.ganza@alustudent.com>
2023-05-17 03:41:25 -0700

    Regular expressions

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 argument = ARGV[0]
 matches = argument.scan(/hbt{2,5}n/)
-if matches == argument.scan(/hbt{2}n/} 
-  print "hbtn"
+if matches == argument.scan(/hbt{2}n/) 
+  print "hbttn"
 end
