f63e12c879c5e20c395cf626623ccdd7976136c7
kennyg37 <k.ganza@alustudent.com>
2023-05-17 03:55:11 -0700

    Regular expressions

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -3,5 +3,5 @@ argument = ARGV[0]
 matches = argument.scan(/hbt{2,5}n/)
 matches.each do |match|
   num_ts = match.count("t")
-  print "hbt" + "t" * num_ts + "n"
+  print "hb" + "t" * num_ts + "n"
 end
