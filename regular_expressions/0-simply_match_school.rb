32c14a4751a409f96a780045869d0f953ccb246b
kennyg37 <k.ganza@alustudent.com>
2023-05-17 03:15:09 -0700

    Regular expressions

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,8 +1,4 @@
 #!/usr/bin/env ruby
 input_string = ARGV[0]
-match = input_string.match(/School/)  
-if match
-  print matches.join('School')
-else
-  puts ""
-end
+match = input_string.match(/School/)
+print matches.join('School')
