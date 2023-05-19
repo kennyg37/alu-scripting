da9d67425a9d0a6ffeea4c629e3e9679a97bbd4d
kennyg37 <k.ganza@alustudent.com>
2023-05-19 02:44:15 -0700

    Regular expressions

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,12 +1,2 @@
 #!/usr/bin/env ruby
-# Read transactions from a file
-transactions_file = ARGV[0]
-
-# Process transactions from the file and extract required information
-File.foreach(transactions_file) do |transaction|
-  sender = transaction.match(/From: (.*?),/)&.captures&.first
-  receiver = transaction.match(/To: (.*?),/)&.captures&.first
-  flags = transaction.match(/Flags: (.*?)$/)&.captures&.first
-
-  puts "#{sender},#{receiver},#{flags}" if sender && receiver && flags
-end
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
